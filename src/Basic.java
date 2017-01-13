650c7d0770209344ce999e2195ec6abd9ba7eefa
Galina Yakovenko <galinayakovenko@galinas-air.gateway.sonic.net>
2017-01-12 19:38:10 -0800

    First Commit

---
+++ src/Basic.java
@@ -0,0 +1,29 @@
+/**
+ *   File Name: Basic.java<br>
+ *
+ *   Yakovenko, Galina<br>
+ *   Created: Jan 12, 2017
+ *
+ */
+
+/**
+ * Basic //ADD (description of class)
+ * <p>
+ * //ADD (description of core fields)
+ * <p>
+ * //ADD (description of core methods)
+ *
+ * @author Yakovenko, Galina
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Basic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/com/sqa/gy/SlightlyLessBasic.java
@@ -0,0 +1,42 @@
+/**
+ *   File Name: SlightlyLessBasic.java<br>
+ *
+ *   Yakovenko, Galina<br>
+ *   Created: Jan 12, 2017
+ *
+ */
+
+package com.sqa.gy;
+
+import org.junit.*;
+
+/**
+ * SlightlyLessBasic //ADD (description of class)
+ * <p>
+ * //ADD (description of core fields)
+ * <p>
+ * //ADD (description of core methods)
+ *
+ * @author Yakovenko, Galina
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class SlightlyLessBasic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TOD Auto-generated method stub
+
+	}
+
+	@Test
+	public void myTest() {
+		// This thing plus the import tells eclipse to give us option to run as
+		// JUNIT test
+
+	}
+
+}
